91f9b4bf19da8b67bd69c88defc030ea42995674
JHBDVT\mmsomi2 <mmsomi2@jhb.dvt.co.za>
2023-07-12 16:52:50 +0200

    Add project files.

---
+++ BlazorShopApp/Server/Controllers/ProductsController.cs
@@ -0,0 +1,27 @@
+﻿using BlazorShopApp.Server.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlazorShopApp.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly DataContext _dataContext;
+
+        public ProductsController(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetProducts()
+        {
+            var products = await _dataContext.Products.ToListAsync();
+            return Ok(products);
+
+        }
+    }
+}
