ce88fdeeaa2934e59fc0ff3c57e9487cacf5eac3
JHBDVT\mmsomi2 <mmsomi2@jhb.dvt.co.za>
2023-07-13 16:10:09 +0200

    Get Products by Category on the Client

--- BlazorShopApp/Client/Services/ProductService/ProductService.cs
+++ BlazorShopApp/Client/Services/ProductService/ProductService.cs
@@ -14,20 +14,27 @@ namespace BlazorShopApp.Client.Services.ProductService
 
         public List<Product> Products { get; set; } = new List<Product>();
 
+        public event Action ProductsChanged;
+
         public async Task<ServiceResponse<Product>> GetProduct(int id)
         {
             var result = await _httpClient.GetFromJsonAsync<ServiceResponse<Product>>($"api/products/{id}");
             return result;
         }
 
-        public async Task GetProducts()
+        public async Task GetProducts(string? categoryUrl = null)
         {
-            var result = await _httpClient.GetFromJsonAsync<ServiceResponse<List<Product>>>("api/products");
+
+            var result = categoryUrl == null ? await _httpClient.GetFromJsonAsync<ServiceResponse<List<Product>>>("api/products")
+                : await _httpClient.GetFromJsonAsync<ServiceResponse<List<Product>>>($"api/products/categories/{categoryUrl}");
             if (result is not null && result.Data is not null)
             {
                 Products = result.Data;
             }
+            ProductsChanged.Invoke();
 
         }
+
+
     }
 }
