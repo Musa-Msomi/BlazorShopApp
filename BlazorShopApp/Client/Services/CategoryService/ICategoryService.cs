93dd2ce949e725d0bae250b212d6ff7cad0675f6
JHBDVT\mmsomi2 <mmsomi2@jhb.dvt.co.za>
2023-07-13 15:02:14 +0200

    added category items to nav sidebar

--- BlazorShopApp/Client/Services/CategoryService/ICategoryService.cs
+++ BlazorShopApp/Client/Services/CategoryService/ICategoryService.cs
@@ -4,7 +4,7 @@ namespace BlazorShopApp.Client.Services.CategoryService
 {
     public interface ICategoryService
     {
-        List<Category> Categories { get; }
+        List<Category> Categories { get; set; }
         Task  GetCategories();
     }
 }
