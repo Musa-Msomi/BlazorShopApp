8473fb94b6bc72a2ccaf79e40923a7d69f548493
JHBDVT\mmsomi2 <mmsomi2@jhb.dvt.co.za>
2023-07-13 12:45:57 +0200

    added functionality for getting product from server in the client project. Moved reused css classes to app.css

--- BlazorShopApp/Client/Services/ProductService/ProductService.cs
+++ BlazorShopApp/Client/Services/ProductService/ProductService.cs
@@ -14,6 +14,12 @@ namespace BlazorShopApp.Client.Services.ProductService
 
         public List<Product> Products { get; set; } = new List<Product>();
 
+        public async Task<ServiceResponse<Product>> GetProduct(int id)
+        {
+            var result = await _httpClient.GetFromJsonAsync<ServiceResponse<Product>>($"api/products/{id}");
+            return result;
+        }
+
         public async Task GetProducts()
         {
             var result = await _httpClient.GetFromJsonAsync<ServiceResponse<List<Product>>>("api/products");
