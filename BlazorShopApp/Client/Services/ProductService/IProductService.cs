3c461045362fd04fac4b61b6fa382ab7f6294637
JHBDVT\mmsomi2 <mmsomi2@jhb.dvt.co.za>
2023-07-14 15:13:35 +0200

    Add Search Methods to the Client

--- BlazorShopApp/Client/Services/ProductService/IProductService.cs
+++ BlazorShopApp/Client/Services/ProductService/IProductService.cs
@@ -4,9 +4,14 @@ namespace BlazorShopApp.Client.Services.ProductService
 {
     public interface IProductService
     {
+        string Message { get; set; }
         event Action ProductsChanged;
         List<Product> Products { get; set; }
         Task GetProducts(string? categoryUrl = null);
         Task<ServiceResponse<Product>> GetProduct(int id);
+        Task SearchProducts(string searchText);
+        Task<List<string>> GetProductSearchSuggestions(string searchText);
+
+
     }
 }
--- BlazorShopApp/Client/Services/ProductService/ProductService.cs
+++ BlazorShopApp/Client/Services/ProductService/ProductService.cs
@@ -13,6 +13,7 @@ namespace BlazorShopApp.Client.Services.ProductService
         }
 
         public List<Product> Products { get; set; } = new List<Product>();
+        public string Message { get; set; } = "loading products...";
 
         public event Action ProductsChanged;
 
@@ -35,6 +36,28 @@ namespace BlazorShopApp.Client.Services.ProductService
 
         }
 
+        public async Task<List<string>> GetProductSearchSuggestions(string searchText)
+        {
+            var result = await _httpClient.GetFromJsonAsync<ServiceResponse<List<string>>>($"api/products/search-suggestions/{searchText}");
 
+            return result.Data;
+        }
+
+        public async Task SearchProducts(string searchText)
+        {
+            var result = await _httpClient.GetFromJsonAsync<ServiceResponse<List<Product>>>($"api/products/search/{searchText}");
+
+            if (result is not null && result.Data is not null)
+            {
+                Products = result.Data;
+            }
+
+            if(Products.Count == 0)
+            {
+                Message = "No product found";
+            }
+
+            ProductsChanged?.Invoke();  
+        }
     }
 }
