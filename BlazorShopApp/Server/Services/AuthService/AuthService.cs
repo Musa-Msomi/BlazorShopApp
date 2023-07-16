ce9104fa4966fef5bbe1f779549e3332864c27df
JHBDVT\mmsomi2 <mmsomi2@jhb.dvt.co.za>
2023-07-16 15:18:04 +0200

    Checking for existing user

--- BlazorShopApp/Server/Services/AuthService/AuthService.cs
+++ BlazorShopApp/Server/Services/AuthService/AuthService.cs
@@ -1,17 +1,35 @@
-﻿using BlazorShopApp.Shared;
+﻿using BlazorShopApp.Server.Data;
+using BlazorShopApp.Shared;
+using Microsoft.EntityFrameworkCore;
 
 namespace BlazorShopApp.Server.Services.AuthService
 {
     public class AuthService : IAuthService
     {
+        private readonly DataContext _dataContext;
+
+        public AuthService(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+
         public Task<ServiceResponse<int>> Register(User user, string password)
         {
             throw new NotImplementedException();
         }
 
-        public Task<bool> UserExists(string email)
+        public async Task<bool> UserExists(string email)
         {
-            throw new NotImplementedException();
+            var userExists = await _dataContext.Users.AnyAsync(x => x.Email.ToLower().Equals(email.ToLower()));
+
+            if (userExists)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
     }
 }
