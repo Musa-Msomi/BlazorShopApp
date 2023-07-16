4790e0ed097c43f7209ce819beabb4da842ee347
JHBDVT\mmsomi2 <mmsomi2@jhb.dvt.co.za>
2023-07-16 15:35:57 +0200

    Added Auth Controller and implemented user registration endpoint

---
+++ BlazorShopApp/Server/Controllers/AuthController.cs
@@ -0,0 +1,36 @@
+﻿using BlazorShopApp.Server.Services.AuthService;
+using BlazorShopApp.Shared;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlazorShopApp.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthService _authService;
+
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+        [HttpPost("register")]
+        public async Task<ActionResult<ServiceResponse<int>>> Register(UserRegister userRegister)
+        {
+            var response = await _authService.Register(new User
+            {
+                Email = userRegister.Email
+            },
+            userRegister.Password
+            );
+
+            if (!response.Success)
+            {
+                return BadRequest(response);
+            }
+
+            return Ok(response);
+        }
+    }
+}
